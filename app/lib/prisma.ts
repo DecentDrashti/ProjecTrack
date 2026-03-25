fb042a5ffdaa7e695717212cfd2c76ecf534d911
DecentDrashti <drashtiruparelia.678@gmail.com>
2026-03-25 09:10:28 +0530

    updated code

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,17 +1,28 @@
+import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb"; 
-import { PrismaClient } from "@/app/generated/prisma/client"; 
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
- 
-const adapter = new PrismaMariaDb({ 
-    host:"localhost", 
-    port:3306, 
-    user:"root", 
-    password:"", 
-    database:"project_track_db", 
-    connectionLimit:5 
-}) 
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+const globalForPrisma = global;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma;
+// import { PrismaMariaDb } from "@prisma/adapter-mariadb"; 
+// import { PrismaClient } from "@/app/generated/prisma/client"; 
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+ 
+// const adapter = new PrismaMariaDb({ 
+//     host:"localhost", 
+//     port:3306, 
+//     user:"root", 
+//     password:"", 
+//     database:"project_track_db", 
+//     connectionLimit:5 
+// }) 
+ 
+// export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
